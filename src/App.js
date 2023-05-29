a056ad71d85737f7c6c70ef4bcd717b7c959d503
Jeff <jeffs7tao@gmail.com>
2023-05-29 00:25:26 -0400

    lint

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
+import "./App.css";
 import React, { useState, useEffect, useRef } from "react";
 import boxes from "./boxes";
-import Box from "./Box"
+import Box from "./Box";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVolumeUp, faVolumeMute } from "@fortawesome/free-solid-svg-icons";
 
-import clickSound from './sounds/boop.wav';
+import clickSound from "./sounds/boop.wav";
 
 function App() {
-
-  const [squares, setSquares] = useState(boxes)
-  const [score, setScore] = useState(0)
+  const [squares, setSquares] = useState(boxes);
+  const [score, setScore] = useState(0);
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [speedMultiplier, setSpeedMultiplier] = useState(1);
   const [gameOver, setGameOver] = useState(false);
@@ -26,38 +25,37 @@ function App() {
   }, []);
 
   function toggle(id) {
-    setSquares(prevSquares => {
-        const newSquares = []
-        for(let i = 0; i < prevSquares.length; i++) {
-            const currentSquare = prevSquares[i]
-            if(currentSquare.id === id) {
-                const updatedSquare = {
-                    ...currentSquare,
-                    on: !currentSquare.on
-                }
-                newSquares.push(updatedSquare)
-            } else {
-                newSquares.push(currentSquare)
-            }
+    setSquares((prevSquares) => {
+      const newSquares = [];
+      for (let i = 0; i < prevSquares.length; i++) {
+        const currentSquare = prevSquares[i];
+        if (currentSquare.id === id) {
+          const updatedSquare = {
+            ...currentSquare,
+            on: !currentSquare.on,
+          };
+          newSquares.push(updatedSquare);
+        } else {
+          newSquares.push(currentSquare);
         }
-        return newSquares
-    })
+      }
+      return newSquares;
+    });
   }
 
-
-  const squareElements = squares.map(square => (
-    <Box 
-        key={square.id} 
-        id={square.id}
-        on={square.on} 
-        toggle={() => handleButtonClick(square.id, square.on)}
+  const squareElements = squares.map((square) => (
+    <Box
+      key={square.id}
+      id={square.id}
+      on={square.on}
+      toggle={() => handleButtonClick(square.id, square.on)}
     />
-  ))
+  ));
 
-  function startEvents(){
-    clearSquares()
-    setTimeElapsed(0) // Reset timer
-    startGame()
+  function startEvents() {
+    clearSquares();
+    setTimeElapsed(0); // Reset timer
+    startGame();
     setGameOver(false); // Reset game over state
   }
 
@@ -69,14 +67,14 @@ function App() {
     let timer;
     if (isGameStarted) {
       timer = setInterval(() => {
-        setSquares(prevSquares => {
+        setSquares((prevSquares) => {
           const randomIndex = Math.floor(Math.random() * prevSquares.length);
           const updatedSquares = prevSquares.map((square, index) => ({
             ...square,
-            on: index === randomIndex ? true : square.on
+            on: index === randomIndex ? true : square.on,
           }));
           // Check if all squares are on
-          if(updatedSquares.every(square => square.on)) {
+          if (updatedSquares.every((square) => square.on)) {
             setGameOver(true);
             clearInterval(timer);
           }
@@ -90,11 +88,10 @@ function App() {
     };
   }, [isGameStarted, speedMultiplier]);
 
-
   function clearSquares() {
-    const clearedSquares = squares.map(square => ({
+    const clearedSquares = squares.map((square) => ({
       ...square,
-      on: false
+      on: false,
     }));
     setSquares(clearedSquares);
     setScore(0); // Reset score counter
@@ -114,19 +111,19 @@ function App() {
       if (newScore > highestScore) {
         setHighestScore(newScore);
       }
-      if (newScore > 50 && newScore % 6 === 0 ){
-        setSpeedMultiplier(prevMultiplier => prevMultiplier * 1.04);
+      if (newScore > 50 && newScore % 6 === 0) {
+        setSpeedMultiplier((prevMultiplier) => prevMultiplier * 1.04);
       }
       if (newScore <= 50 && newScore > 20 && newScore % 3 === 0) {
-        setSpeedMultiplier(prevMultiplier => prevMultiplier * 1.08);
+        setSpeedMultiplier((prevMultiplier) => prevMultiplier * 1.08);
       }
       if (newScore <= 20 && newScore % 3 === 0) {
-        setSpeedMultiplier(prevMultiplier => prevMultiplier * 1.15);
+        setSpeedMultiplier((prevMultiplier) => prevMultiplier * 1.15);
       }
     } else {
       // Ensure score never falls below 0
       if (score > 4) {
-        setScore(prevScore => prevScore - 5);
+        setScore((prevScore) => prevScore - 5);
       } else {
         setScore(0);
       }
@@ -135,54 +132,62 @@ function App() {
 
   useEffect(() => {
     let interval;
-  
+
     if (isGameStarted) {
       const startTime = Date.now() - timeElapsed;
       interval = setInterval(() => {
         setTimeElapsed(Date.now() - startTime);
       }, 1);
     }
-  
+
     if (score >= 50 || gameOver) {
       clearInterval(interval);
       if (timeElapsed < shortestTime) {
         setShortestTime(timeElapsed);
       }
     }
-  
+
     return () => {
       clearInterval(interval);
     };
   }, [isGameStarted, score, gameOver, timeElapsed, shortestTime]);
-  
 
   const formatTime = (time) => {
     const milliseconds = Math.floor(time % 1000);
     const seconds = Math.floor(time / 1000);
-  
-    return `${seconds}.${milliseconds < 10 ? '00' + milliseconds : milliseconds < 100 ? '0' + milliseconds : milliseconds}`;
+
+    return `${seconds}.${
+      milliseconds < 10
+        ? "00" + milliseconds
+        : milliseconds < 100
+        ? "0" + milliseconds
+        : milliseconds
+    }`;
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <a className="textDiv">
-          A click-based reaction game, inspired by ones used by professional athletes to test reflexes.
-          <br></br><br></br>
+          A click-based reaction game, inspired by ones used by professional
+          athletes to test reflexes.
+          <br></br>
+          <br></br>
           <strong>How to play? </strong>
-          Click green buttons before the grid fills up. Avoid wrong squares for minus points. The game will speed up so be quick. 
+          Click green buttons before the grid fills up. Avoid wrong squares for
+          minus points. The game will speed up so be quick.
         </a>
       </header>
       <br></br>
-      <body  className="bodyDiv">
+      <body className="bodyDiv">
         <div className="gamePanel">
           <div className="scorePanel">
             <span>Current Score: {score}</span>
-            <span>Highest Score: {highestScore}</span> 
+            <span>Highest Score: {highestScore}</span>
           </div>
           {gameOver && <div className="gameOver">GAME OVER</div>}
           <div>
-            <div onClick={() => setIsSoundEnabled(prevState => !prevState)}>
+            <div onClick={() => setIsSoundEnabled((prevState) => !prevState)}>
               {isSoundEnabled ? (
                 <FontAwesomeIcon icon={faVolumeUp} />
               ) : (
@@ -190,14 +195,23 @@ function App() {
               )}
             </div>
           </div>
-          <button onClick={startEvents} className="startButton">Click to Start</button>
+          <button onClick={startEvents} className="startButton">
+            Click to Start
+          </button>
         </div>
-        
+
         <div>{squareElements}</div>
-        
+
         <div className="stopWatch">
           <span>Elapsed time to 50-pts: {formatTime(timeElapsed)}</span>
-          <span><div>Shortest time: {shortestTime === Number.MAX_SAFE_INTEGER ? 'N/A' : formatTime(shortestTime)}</div></span>
+          <span>
+            <div>
+              Shortest time:{" "}
+              {shortestTime === Number.MAX_SAFE_INTEGER
+                ? "N/A"
+                : formatTime(shortestTime)}
+            </div>
+          </span>
         </div>
       </body>
       {gameOver && <div className="gameOverMobile">GAME OVER</div>}
@@ -206,4 +220,3 @@ function App() {
 }
 
 export default App;
-
--- src/Box.js
+++ src/Box.js
@@ -1,30 +1,28 @@
 import React, { useState, useEffect } from "react";
 
 export default function Box(props) {
+  const [transientColor, setTransientColor] = useState("transparent");
 
-    const [transientColor, setTransientColor] = useState("transparent");
-
-    useEffect(() => {
-        if (!props.on) {
-            setTransientColor("lightgreen");
-            setTimeout(() => {
-                setTransientColor("transparent");
-            }, 100);
-        } else {
-            setTransientColor("#355E3B");
-        }
-      }, [props.on]);
-
-    const styles = {
-        backgroundColor: props.on ? "#355E3B" : transientColor
+  useEffect(() => {
+    if (!props.on) {
+      setTransientColor("lightgreen");
+      setTimeout(() => {
+        setTransientColor("transparent");
+      }, 100);
+    } else {
+      setTransientColor("#355E3B");
     }
+  }, [props.on]);
 
-    return (
-        <div 
-            style={styles} 
-            className="box"
-            onClick={()=>props.toggle(props.id)}
-        >
-        </div>
-    )
+  const styles = {
+    backgroundColor: props.on ? "#355E3B" : transientColor,
+  };
+
+  return (
+    <div
+      style={styles}
+      className="box"
+      onClick={() => props.toggle(props.id)}
+    ></div>
+  );
 }
